3f055c6a805f0d0115076e42d61ee01a922983e8
gianluca <gianluca.most@innoq.com>
2018-05-01 14:49:12 +0200

    initial commit of prototype as seen in Kaunas

---
+++ Assets/BehaviourScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BehaviourScript : MonoBehaviour {
+
+	public Image image;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		if(DefaultTrackableEventHandler.isTracked) {
+			// hide
+			image.enabled = false;
+		} else {
+			image.enabled = true;
+		}
+	}
+}
